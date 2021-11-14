c6d2fa73ba7ca45a1f85b6c96c16c771e82054d2
kocagozhkn <kocagoz@gmail.com>
2021-11-14 14:04:42 +0300

    usersh git link added

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -5,5 +5,5 @@ pip3 install flask
 pip3 install flask_mysql
 yum install git -y
 TOKEN="ghp_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/MiranaSGit/phonebook.git
+cd /home/ec2-user && git clone https://$TOKEN@github.com/kocagozhkn/DevOps_Project_02-ELB-and-AS-AppDeployment-with-Terraform.git
 python3 /home/ec2-user/phonebook/phonebook-app.py
